f9ea66b6b699b22efec4e1e480ad45ef621b9261
RASHI-AI7 <rashisingh.bt@gmail.com>
2025-05-29 00:19:46 +0530

    Added Floyd's triangle

--- invertedpattern.java
+++ invertedpattern.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class invertedpattern {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the number of rows: ");
+        System.out.print("Enter the number of rows: ");
         int n = scan.nextInt();
         for (int i=n; i>=1; i--){
             for(int j=1; j<=i; j++){
@@ -11,6 +11,8 @@ public class invertedpattern {
             }
         System.out.println();
         }
+
+         
         scan.close();
     }
 }
