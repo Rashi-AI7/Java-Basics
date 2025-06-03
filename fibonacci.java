4ce653f272353648b0cbd27808bbda8ebc983ef5
RASHI-AI7 <rashisingh.bt@gmail.com>
2025-06-03 12:17:55 +0530

    Added fibonacci series

---
+++ fibonacci.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        fibo(n);
+        sc.close();
+    }
+
+    public static void fibo(int n){
+        int first = 0;
+        int second = 1;
+        System.out.print("Fibonacci series till " + n + " terms: ");
+        for(int i=0; i<=n; i++){
+            System.out.print( first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
