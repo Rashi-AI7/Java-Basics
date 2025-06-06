c4d4d41ff66d6d33d5bb68b78bdc756d4cedaa5f
RASHI-AI7 <rashisingh.bt@gmail.com>
2025-06-06 14:03:32 +0530

    Added sum of first n  natural numbers

--- recursion.java
+++ recursion.java
@@ -6,7 +6,7 @@ public class recursion {
         System.out.print("Enter any number to get reverse counting: ");
         int n = sc.nextInt();
         printBack(n);
-
+        recursion2.printnum(sc);
         
         sc.close();
     }
@@ -17,4 +17,22 @@ public class recursion {
             System.out.println(n);
             printBack(n-1);
         }
+}
+
+class recursion2 {
+    public static void printnum(Scanner sc){
+        System.out.println("Enter any natural number to get it's sum: ");
+        int m = sc.nextInt();
+        int sum = 0;
+        printsum(m, sum);
+    }
+    public static void printsum(int m, int sum){
+        if(m==0){
+            System.out.println(sum);
+            return;
+        }
+        
+        sum+=m;
+        printsum(m-1, sum);
+    }
 }
