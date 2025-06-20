bfd0bdba59d94e2542d37ba4d308dd27af20ae32
RASHI-AI7 <rashisingh.bt@gmail.com>
2025-06-20 13:42:06 +0530

    Added power calculator using recursion

---
+++ calcpower.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class calcpower {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter x: ");
+        int x = sc.nextInt();
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        System.out.print(x + " raise to the power " + n + " is " + calcpow(x, n));
+    
+    }
+
+    public static int calcpow(int x, int n){
+        if(x==0){
+            return 0;
+        }if(n==0){
+            return 1;
+        }
+        int xpownm1 = calcpow(x, n-1);
+        int xpown = x * xpownm1;
+        return xpown;
+    }
+}
