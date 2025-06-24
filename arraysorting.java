4dbc2c908969d7b7ecb6ebfc563b0b932ddd7959
RASHI-AI7 <rashisingh.bt@gmail.com>
2025-06-24 23:59:56 +0530

    Added

---
+++ arraysorting.java
@@ -0,0 +1,59 @@
+// import java.util.Scanner;
+
+// public class arraysorting{
+//     public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int size = sc.nextInt();
+//     int arr[] = new int[size];
+
+//     for(int i=0; i<size; i++){
+//         arr[i] = sc.nextInt();
+//     }
+
+//     //output
+//     System.out.println(isSorted(arr, size));
+//     sc.close();
+// }
+
+//     public static boolean isSorted(int arr[], int idx){
+//         if(idx==arr.length){
+//             return true;
+//         }
+
+//         if(arr[idx] < arr[idx+1]){
+//             return isSorted(arr, idx+1);
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+// }
+
+import java.util.Scanner;
+
+public class arraysorting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(isSorted(arr, 0));
+        sc.close();
+    }
+
+    public static boolean isSorted(int[] arr, int idx) {
+        if (idx >= arr.length - 1) {
+            return true;
+        }
+
+        if (arr[idx] < arr[idx + 1]) {
+            return isSorted(arr, idx + 1);
+        } else {
+            return false;
+        }
+    }
+}
