ee3b9502e64c2310e2ffc3782fa1f6c0a56b8908
RASHI-AI7 <rashisingh.bt@gmail.com>
2025-06-27 21:52:03 +0530

    Added about constructor using this

--- oops.java
+++ oops.java
@@ -1,16 +1,21 @@
 class Student{
     String name;
     int age;
+
+    //classMethod
     public void myMethod(){
         System.out.println("Hello World!");
     }
-}
 
+    //constructor
+    public Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+}
 public class oops {
     public static void main(String[] args){
-        Student s1 = new Student(); //creating object
-        s1.name = "Garv";
-        s1.age = 12;
+        Student s1 = new Student("Garv", 12); //creating object
         s1.myMethod();
         System.out.println(s1.name);
         System.out.println(s1.age);
