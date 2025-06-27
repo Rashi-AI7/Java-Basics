142e1eaa081dff50cefba018a276f39aed6fe568
RASHI-AI7 <rashisingh.bt@gmail.com>
2025-06-27 21:30:55 +0530

    Added about classMethod

--- oops.java
+++ oops.java
@@ -1,6 +1,9 @@
 class Student{
     String name;
     int age;
+    public void myMethod(){
+        System.out.println("Hello World!");
+    }
 }
 
 public class oops {
@@ -8,6 +11,7 @@ public class oops {
         Student s1 = new Student(); //creating object
         s1.name = "Garv";
         s1.age = 12;
+        s1.myMethod();
         System.out.println(s1.name);
         System.out.println(s1.age);
     }
