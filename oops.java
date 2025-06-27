5c558040b30ed9876fae8315364826b099325470
RASHI-AI7 <rashisingh.bt@gmail.com>
2025-06-27 20:52:43 +0530

    Added object creation in class

---
+++ oops.java
@@ -0,0 +1,14 @@
+class Student{
+    String name;
+    int age;
+}
+
+public class oops {
+    public static void main(String[] args){
+        Student s1 = new Student(); //creating object
+        s1.name = "Garv";
+        s1.age = 12;
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+    }
+}
