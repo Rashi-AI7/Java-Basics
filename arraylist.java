41ed6ac5ad47c2cd46f886baab5d8fd1068d91f4
RASHI-AI7 <rashisingh.bt@gmail.com>
2025-07-01 20:37:05 +0530

    Added about arraylist

---
+++ arraylist.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class arraylist {
+    public static void main(String[] args) {
+    //create and display
+    ArrayList <Integer> arr = new ArrayList<>(5);
+    arr.add(2);
+    arr.add(4);
+    arr.add(6);
+    arr.add(8);
+    arr.add(10);
+    
+    //update 
+    ArrayList <String> clr = new ArrayList<>();
+    clr.add("Red");
+    clr.add("Pink");
+    clr.add("Blue");
+    clr.add("Green");
+    clr.set(2, "Orange");
+
+    System.out.println("Evn numbers: " + arr);
+    System.out.println("Colours: " + clr);
+    }
+}
