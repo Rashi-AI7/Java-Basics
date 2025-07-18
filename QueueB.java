c2fb5e97e88a01e2f4971b8d6289e20a1e95002d
RASHI-AI7 <rashisingh.bt@gmail.com>
2025-07-18 19:30:49 +0530

    Add queue using array

---
+++ QueueB.java
@@ -0,0 +1,54 @@
+public class QueueB {
+    static class Queue {
+        int arr[];
+        int size;
+        int rear;
+
+        Queue(int size){
+            this.size = size;
+            arr = new int[size];
+            rear = -1;
+        }
+        public boolean isEmpty(){
+            return rear == -1;
+        }
+        public boolean isFull(){
+            return rear == size-1;
+        }
+        public void add(int data){
+            if(isFull()){
+                System.out.println("Overflow");
+                return;
+            }
+            arr[++rear] = data;
+        }
+        public int remove(){
+            if(isEmpty()){
+                System.out.println("Empty Queue");
+                return -1;
+            }
+            int front = arr[0];
+            for(int i=0; i<rear; i++){
+                arr[i] = arr[i+1];
+            }
+            rear--;
+            return front;
+        }
+        public int peek(){
+            if(isEmpty()){
+                System.out.println("Empty Queue");
+                return -1;
+            }
+            return arr[0];
+        }
+    }
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        System.out.println(q.remove());
+        System.out.println(q.peek());
+    }
+}
