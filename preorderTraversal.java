7c18dd518b4d019f35331c8d4f94cca7f583386a
RASHI-AI7 <rashisingh.bt@gmail.com>
2025-08-03 23:57:33 +0530

    Add Preorder in trees

---
+++ preorderTraversal.java
@@ -0,0 +1,29 @@
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val){
+        this.val = val;
+    }
+}
+
+public class preorderTraversal {
+    static void preOrder(TreeNode root){
+        if(root == null)
+        return;
+        System.out.println(root.val + "");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        preOrder(root);
+    }
+}
