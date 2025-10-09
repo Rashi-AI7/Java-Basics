7af83f68cd2fbfb4b34ec3d7dbf01af79c6312d7
Rashi Singh <rashisingh.bt@gmail.com>
2025-10-09 09:19:42 +0530

    Create nrotation.java

---
+++ nrotation.java
@@ -0,0 +1,28 @@
+class nrotation {
+    public int findKRotation(int[] nums) {
+        int low = 0, high = nums.length-1;
+        int ans = Integer.MIN_VALUE;
+        int index = -1;
+        while(low<=high){
+            if(nums[low] < nums[high]){
+                index = low;
+                ans = nums[low];
+            }
+            break;
+            int mid = (low+high)/2;
+            if(nums[mid]<nums[low]){
+                if(nums[mid]<ans){
+                    index = mid;
+                    ans = nums[mid];
+                }high = mid-1;
+            }else //nums[mid]>=nums[low]
+            { if(nums[mid]<ans){
+                index = mid;
+                ans = nums[mid];
+            }low = mid+1;
+            }
+        }
+    return index;
+    }
+}
+}
